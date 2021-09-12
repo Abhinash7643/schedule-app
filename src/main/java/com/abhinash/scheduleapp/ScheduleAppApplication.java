afb8cc9e813640fd9bfccabd0d79252a03d7733c
Abhinash Kumar <abhinash7643@gmail.com>
2021-09-12 19:17:38 +0200

    initial index page

---
+++ src/main/java/com/abhinash/scheduleapp/ScheduleAppApplication.java
@@ -0,0 +1,13 @@
+package com.abhinash.scheduleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abhinash/scheduleapp/ScheduleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhinash.scheduleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
